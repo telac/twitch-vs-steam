76c918ab6b672424b58fd74d8369d88b4c29864e
Christian Westerlund <ca.westerlund@gmail.com>
2018-08-03 00:02:59 +0300

    horrible self-contained timer to fetching data

--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ import csv
 import codecs
 import requests
 import sqlite3
+import sys
 from time import sleep
 from configparser import ConfigParser
 from urllib.request import urlopen
@@ -114,13 +115,34 @@ class APIconnector():
 
         return viewers_by_game
 
+def fetch_data():
+        ac = APIconnector(True)
+        dbconnector = DatabaseConnector(steam=ac.steam, twitch=ac.twitch, current_time=ac.current_time, batchID=ac.batchID)
+        dbconnector.create_schema()
+        dbconnector.insert_data()
+
+def plot_data():
+        da = DataAnalyzer()
+        # da.show()
+        da.plot_data()
+        da.plot_ratio()
+        da.plot_order()
+
 if __name__ == "__main__":
-    ac = APIconnector(True)
-    dbconnector = DatabaseConnector(steam=ac.steam, twitch=ac.twitch, current_time=ac.current_time, batchID=ac.batchID)
-    dbconnector.create_schema()
-    dbconnector.insert_data()
-    da = DataAnalyzer()
-    da.show()
-    da.plot_data()
-    da.plot_ratio()
-    da.plot_order()
+    if len(sys.argv) > 3:
+        fetch = sys.argv[1]
+        plot = sys.argv[2]
+        fetch_timer = int(sys.argv[3])
+        while True:
+            if fetch == '1':
+                fetch_data()
+            if plot == '1':
+                plot_data()
+            sleep(fetch_timer)
+
+    else:
+        print("no arguments given, fetching and plotting")
+        print("example arg: get_data.py 1 1 900 (fetches and plots data, fetches new data every 15 minutes)")
+        print("example arg: get_data.py 1 0 900 (fetches, fetches new data every 15 minutes)")
+        fetch_data()
+        plot_data()
